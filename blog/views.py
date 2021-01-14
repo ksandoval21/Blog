0e49965d664539815b3cc33d65ac1209938bfce7
ksandoval21 <ksandoval-lopez21@basecampcodingacademy.org>
2021-01-14 11:08:48 -0600

    complete chapter

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
+from django.urls import reverse_lazy 
 from .models import Post
 class BlogListView(ListView):
-    model=Post
-    template_name='home.html'
+    model = Post
+    template_name = 'home.html'
 class BlogDetailView(DetailView):
-    model=Post
-    template_name='post_detail.html'
+    model = Post
+    template_name = 'post_detail.html'
+class BlogCreativeView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
